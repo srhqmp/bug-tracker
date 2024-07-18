b41a7503e6624cae80f59c3feffb5efa869980da
srhqmp <ballanosarah@gmail.com>
2024-07-18 23:49:01 +0800

    feat: use recharts

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,18 +5,20 @@ import Logo from "./logo.png";
 
 export default function Nav() {
   return (
-    <nav>
-      <Image
-        src={Logo}
-        alt="bug tracker logo"
-        width={80}
-        placeholder="blur"
-        quality={100}
-      />
-      <h1>Bug Tracker</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
-      <Link href="/tickets/new">Create New</Link>
-    </nav>
+    <main>
+      <nav>
+        <Image
+          src={Logo}
+          alt="bug tracker logo"
+          width={80}
+          placeholder="blur"
+          quality={100}
+        />
+        <h2>Bug Tracker</h2>
+        <Link href="/">Dashboard</Link>
+        <Link href="/tickets">Tickets</Link>
+        <Link href="/tickets/new">Create New</Link>
+      </nav>
+    </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
-export default function Home() {
+import { getTickets } from "@/lib/utils";
+import { Ticket } from "@/lib/definitions";
+
+import TicketsList from "./components/tickets";
+import IssuesChart from "./components/issuesChart";
+
+export default async function Home() {
+  const tickets = await getTickets();
+  const high = tickets.filter((ticket: Ticket) => ticket.severity === "high");
+  const medium = tickets.filter(
+    (ticket: Ticket) => ticket.severity === "medium"
+  );
+  const low = tickets.filter((ticket: Ticket) => ticket.severity === "low");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Home</main>
+    <main>
+      <section>
+        <h2>Overview</h2>
+        <div className="card">
+          <div className="issues">
+            <div className="count">{tickets.length}</div>
+            <h3>Total Issues</h3>
+          </div>
+          <div className="issues">
+            <div className="count">{tickets.length}</div>
+            <h3>Open Issues</h3>
+          </div>
+          <div className="issues">
+            <div className="count">0</div>
+            <h3>Resolved Issues Today</h3>
+          </div>
+        </div>
+
+        <h2>Issue Statistics</h2>
+        <IssuesChart />
+      </section>
+    </main>
   );
 }
